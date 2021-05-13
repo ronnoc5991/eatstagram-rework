20256adbbd5c83be1e8eb03734860b6ad49b8832
Connor Streng <connor.streng@mediamonks.com>
2021-05-13 13:07:49 +0200

    Create Form for creating and updating recipes

---
+++ src/components/molecules/Form/Form.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+  border: 1px solid red;
+`;
---
+++ src/components/molecules/Form/Form.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable react/no-array-index-key */
+import { ChangeEvent, useEffect, useState } from 'react';
+import { StyledForm } from './Form.styles';
+import Input from '../../atoms/Input/Input';
+import type { Recipe } from '../../../types/Recipe';
+
+type FormProps = {
+  recipe?: Recipe;
+  onSubmit: (recipe: Recipe) => void;
+};
+
+// for update form, need the ability to delete tags/ingredients/steps
+
+export default function Form({ recipe, onSubmit }: FormProps): JSX.Element {
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [image, setImage] = useState<string>('');
+  const [duration, setDuration] = useState<number>();
+  const [currentIngredient, setCurrentIngredient] = useState<string>('');
+  const [ingredients, setIngredients] = useState<Array<string>>([]);
+  const [currentStep, setCurrentStep] = useState<string>('');
+  const [steps, setSteps] = useState<Array<string>>([]);
+  const [currentTag, setCurrentTag] = useState<string>('');
+  const [tags, setTags] = useState<Array<string>>([]);
+  const [id, setId] = useState<string>('');
+
+  const handleSubmit = () => {
+    onSubmit({
+      title,
+      description,
+      image,
+      duration,
+      ingredients,
+      steps,
+      tags,
+      id,
+    });
+  };
+
+  useEffect(() => {
+    if (recipe) {
+      setTitle(recipe.title);
+      setDescription(recipe.description);
+      setImage(recipe.image);
+      setIngredients(recipe.ingredients);
+      setSteps(recipe.steps);
+      if (recipe.duration) setDuration(recipe.duration);
+      if (recipe.tags) setTags(recipe.tags);
+      if (recipe.id) setId(recipe.id);
+    }
+  }, [recipe]);
+
+  return (
+    <StyledForm>
+      <Input
+        type="text"
+        name="title"
+        autoComplete="off"
+        label="Title"
+        value={title}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setTitle(event.currentTarget.value)}
+      />
+      <Input
+        type="text"
+        name="description"
+        autoComplete="off"
+        label="Description"
+        value={description}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setDescription(event.currentTarget.value)
+        }
+      />
+      <Input
+        type="number"
+        name="duration"
+        autoComplete="off"
+        label="Duration"
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setDuration(parseInt(event.currentTarget.value, 10))
+        }
+      />
+      <Input
+        type="text"
+        name="ingredient"
+        autoComplete="off"
+        label="Ingredients"
+        value={currentIngredient}
+        hasSubmitButton
+        onSubmit={() => {
+          setIngredients([...ingredients, currentIngredient]);
+          setCurrentIngredient('');
+        }}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setCurrentIngredient(event.currentTarget.value)
+        }
+      />
+      <Input
+        type="text"
+        name="step"
+        autoComplete="off"
+        label="Steps"
+        value={currentStep}
+        hasSubmitButton
+        onSubmit={() => {
+          setSteps([...steps, currentStep]);
+          setCurrentStep('');
+        }}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setCurrentStep(event.currentTarget.value)
+        }
+      />
+      <Input
+        type="text"
+        name="tag"
+        autoComplete="off"
+        label="Tags"
+        value={currentTag}
+        hasSubmitButton
+        onSubmit={() => {
+          setTags([...tags, currentTag]);
+          setCurrentTag('');
+        }}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setCurrentTag(event.currentTarget.value)
+        }
+      />
+      <Input type="button" value="Submit" onClick={handleSubmit} />
+    </StyledForm>
+  );
+}
