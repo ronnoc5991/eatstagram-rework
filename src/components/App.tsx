ad88392656ab3e7dbaebf65fe999c9430c9a262a
Connor Streng <connor.streng@mediamonks.com>
2021-05-13 13:10:16 +0200

    Temporarily add recipe getter to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from 'react';
 import Routes from './pages/Routes';
 import Navigation from './organisms/Navigation';
 import { StyledMainContent } from './App.styles';
+import type { Recipe } from '../types/Recipe';
+import { getAndSetRecipes } from '../utils/firebaseFunctions';
 
 export default function App(): JSX.Element {
+  const [recipes, setRecipes] = useState<Array<Recipe>>([]);
+
+  useEffect(() => {
+    // getAndSetRecipes(setRecipes);
+  }, []);
+
+  useEffect(() => {
+    if (recipes) console.log(recipes);
+  }, [recipes]);
+
   return (
     <>
       <Navigation />
