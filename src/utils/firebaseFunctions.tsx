6943ecd719b6ae428711f5c8691f8aba405d14ef
Connor Streng <connor.streng@mediamonks.com>
2021-05-15 11:46:28 +0200

    Add new function for uploading image

--- src/utils/firebaseFunctions.tsx
+++ src/utils/firebaseFunctions.tsx
@@ -3,10 +3,31 @@ import type { Recipe } from '../types/Recipe';
 
 // CRUD functions for recipes
 
+export const uploadImageToFirebase = (messageRef, userId: string, file: File) => {
+  const filePath = `${userId}/${messageRef.id}/${file.name}`;
+  return firebase
+    .storage()
+    .ref(filePath)
+    .put(file)
+    .then((fileSnapshot) => {
+      return fileSnapshot.ref.getDownloadURL().then((url) => {
+        return messageRef.update({
+          imageUrl: url,
+          storageUri: fileSnapshot.metadata.fullPath,
+        });
+      });
+    });
+};
+
 // Create
-export const createRecipe = (recipe: Recipe): void => {
+export const createRecipe = (recipe: Recipe, imageFile: File): void => {
   const database = firebase.firestore();
-  database.collection('recipes').add({ ...recipe });
+  database
+    .collection('recipes')
+    .add({ ...recipe })
+    .then((messageRef) => {
+      uploadImageToFirebase(messageRef, firebase.auth().currentUser?.uid as string, imageFile);
+    });
 };
 
 // Read
@@ -51,4 +72,13 @@ export const updateRecipe = (recipe: Recipe): void => {
 export const deleteRecipe = (recipe: Recipe): void => {
   const database = firebase.firestore();
   database.collection('recipes').doc(recipe.id).delete();
+};
+
+// User info
+export const getUserName = (): string => {
+  return firebase.auth().currentUser?.displayName as string;
+};
+
+export const getUserProfilePicUrl = (): string => {
+  return firebase.auth().currentUser?.photoURL as string;
 };
