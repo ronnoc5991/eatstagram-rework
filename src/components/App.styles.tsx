43722fb08f94d856bdedcfb1844456354bc6d3d9
Connor Streng <connor.streng@mediamonks.com>
2021-05-29 11:38:52 +0200

    Remove unneccessary styles from App

--- src/components/App.styles.tsx
+++ src/components/App.styles.tsx
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const StyledMainContent = styled.main`
   display: grid;
   place-items: center;
-  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+  box-sizing: border-box;
 `;
